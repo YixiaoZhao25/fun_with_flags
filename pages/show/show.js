f27b76c64b4adab081f03e7055cdfe054857e111
Ricardo Rodriguez <ricardo.rocer@gmail.com>
2020-11-26 21:38:20 +0800

    like button with function finished

--- pages/show/show.js
+++ pages/show/show.js
@@ -17,62 +17,40 @@ Page({
         console.log('funfacts',funfacts)
         this.setData({funfacts: funfacts.data.objects})
       })
-
+    })
   },
+  fetchCountry: function (id) {
+    let countries = new wx.BaaS.TableObject('countries')
+    let funfacts = new wx.BaaS.TableObject('funfacts')
+    let recordID = options.id 
+    console.log(recordID)
 
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
+    countries.get(recordID).then(res => {
+      console.log(res)
+      this.setData({countries: res.data})
+      
+    let query = new wx.BaaS.Query()
+    query.compare('country_id', '=', options.id) 
+      funfacts.setQuery(query).expand(['country_id']).find().
+  
+      then(funfacts => {
+        console.log('funfacts',funfacts)
+        this.setData({funfacts: funfacts.data.objects})
+      })
+    })
   },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  backToIndex: function (event) {
-    wx.switchTab({
-      url: '/pages/index/index',
+  countLikes: function () {
+    let Favorite = new wx.BaaS.TableObject("favorites")
+    let data = {
+      user_id: this.data.currentUser.id,
+      country_id: this.data.countries.id
+    }
+    Favorite.create().set(data).save().then(res => {
+      let Countries = new wx.BaaS.TableObject("countries")
+      let country = Countries.getWithoutData(this.data.countries.id)
+      country.set({likednum: this.data.likednum + 1}).update().then(res => {
+        console.log(res)
+      })
     })
   }
-
 })
