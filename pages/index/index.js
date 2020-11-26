f3b946b874a6bfe327944fee5b3472638e785250
Xiway Banh <xiway.banh@gmail.com>
2020-11-26 22:07:23 +0800

    fix show page

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,15 +7,6 @@ Page({
       this.setData({countries: res.data.objects})
     })
   },
-  goToShowPage: function (e){
-    let id = e.currentTarget.dataset.id
-    wx.navigateTo({
-      url: `/pages/show/show?id=${id}1`,
-    })
-    console.log(e)
-    console.log(id)
-    
-  }
   goToCountry: function(e) {
     console.log(e)
     let id= e.currentTarget.dataset.id
@@ -23,5 +14,4 @@ Page({
       url: `/pages/show/show?id=${id}`,
     })
   },
-
 })
