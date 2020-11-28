22f6d8f5c1fc70344b574130b0f0240eee0215a3
Ricardo Rodriguez <ricardo.rocer@gmail.com>
2020-11-28 13:03:12 +0800

    fixed issues on show

--- pages/show/show.js
+++ pages/show/show.js
@@ -1,37 +1,87 @@
 // pages/show/show.js
 Page({
 
-  onLoad: function (options) {
-    this.fetchCountry(options.id)
-  },
-  
   fetchCountry: function (id) {
     let countries = new wx.BaaS.TableObject('countries')
-    let funfacts = new wx.BaaS.TableObject('funfacts')
     countries.get(id).then(res => {
       console.log(res)
-      this.setData({countries: res.data})
-      
-      let query = new wx.BaaS.Query()
-      query.compare('country_id', '=', options.id)
-      funfacts.setQuery(query).expand(['country_id']).find().then(funfacts => {
-        console.log('funfacts',funfacts)
-        this.setData({funfacts: funfacts.data.objects})
-      })
+      let country = res.data
+      this.setData({ country })
     })
   },
-  countLikes: function () {
+
+  fetchFunFacts: function (id) {
+    console.log(id)
+    let query = new wx.BaaS.Query()
+    let funfacts = new wx.BaaS.TableObject('funfacts')
+
+    query.compare("country_id", "=", id) 
+    
+    funfacts.setQuery(query).find().then(res => {
+      if (res.data.objects[0]) {
+        let funFact = res.data.objects[0].description
+        this.setData({ funFact })
+      }
+    })
+  },
+
+  addFavorite: function () {
     let Favorite = new wx.BaaS.TableObject("favorites")
+    let favorite = Favorite.create()
     let data = {
       user_id: this.data.currentUser.id,
-      country_id: this.data.countries.id
+      country_id: this.data.country.id
     }
-    Favorite.create().set(data).save().then(res => {
+    
+    favorite.set(data).save().then(res => {
       let Countries = new wx.BaaS.TableObject("countries")
-      let country = Countries.getWithoutData(this.data.countries.id)
-      country.set({likednum: this.data.likednum + 1}).update().then(res => {
+      let country = Countries.getWithoutData(this.data.country.id)
+      country.set({likednum: this.data.country.likednum + 1}).update().then(res => {
         console.log(res)
       })
     })
+  },
+
+  removeFavorite: function () {
+    let Favorite = new wx.BaaS.TableObject("favorites")
+    let favorite = Favorite.create()
+    let data = {
+      user_id: this.data.currentUser.id,
+      country_id: this.data.country.id
+    }
+    
+    favorite.set(data).save().then(res => {
+      let Countries = new wx.BaaS.TableObject("countries")
+      let country = Countries.getWithoutData(this.data.country.id)
+      country.set({likednum: this.data.country.likednum - 1}).update().then(res => {
+        console.log(res)
+      })
+    })
+  },
+
+  getFavorites: function () {
+    // 1. Query our favorties table for userid and countryid
+    // 2. Set this favorites array to our local data
+    let Favorite = new wx.BaaS.TableObject("favorites")
+    let favorite = Favorite.create()
+    let data = {
+      user_id: this.data.currentUser.id,
+      country_id: this.data.country.id
+    }
+
+    favorite.set(data).save().then(res => {
+      let Favorites = new wx.BaaS.TableObject("favorites")
+      let favorite = Favorites.getWithoutData(this.data.country.id)
+      country.set({likednum: this.data.country.likednum}).update().then(res => {
+        console.log(res)
+      })
+    })
+  },
+
+  onLoad: function (options) {
+    let id = options.id
+    this.fetchCountry(id)
+    this.fetchFunFacts(id)
   }
+
 })
